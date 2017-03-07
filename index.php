7c838b5a390a841229cf004cd9842140f19786a8
Tlangelani Mboweni <mbowenit@gmail.com>
2017-03-07 12:48:32 +0200

    WIP

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
 
-$data = [
-    'info' => [
-        'app' => 'Spree Sharks',
-        'version' => '0.0.1'
-    ]
-];
+require __DIR__ . '/vendor/autoload.php';
 
-echo json_encode($data);
+$app = new \Slim\App();
+
+$app->get('/', function (Request $req,  Response $res, $args = []) {
+    return $res->withStatus(400)->write('Bad Request');
+});
+
+$app->run();
+
+// $data = [
+//     'info' => [
+//         'app' => 'Spree Sharks',
+//         'version' => '0.0.1'
+//     ]
+// ];
+
+// echo json_encode($data);
